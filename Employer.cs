9db2b2e10271af486e2111afe65d6c6c4b373ef7
ponuzsr <ponuzsr@kkszki.hu>
2024-09-19 08:59:52 +0200

    Employer class is ready

---
+++ Employer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public class Employer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age {  get; set; }
+        public int Salary { get; set; }
+    }
+}
